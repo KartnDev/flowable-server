4757eb0040d4bd4bb6d6a46a69a16e15e085a7e5
Kartndev <dmutpuu2000@gmail.com>
2024-05-13 12:43:24 +0300

    Initial commit

---
+++ src/main/java/io/kartondev/flowableserver/FlowableServerApplication.java
@@ -0,0 +1,13 @@
+package io.kartondev.flowableserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableServerApplication.class, args);
+    }
+
+}
